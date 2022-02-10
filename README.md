5596a22117b0a0f6c17b6fe5b3f9f88351cd12bc
Pranjal <pranjalmishra2022@gmail.com>
2022-02-10 17:59:30 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,9 @@
+# CLI-Quiz  
+A CLI Premier league quiz app.  
+
+
+scope of improvments:
+1) Questions on DB
+2) Questions encrypted
+3) More Levels
+4) Hints
---
+++ index.js
@@ -0,0 +1,102 @@
+var readlineSync = require('readline-sync');
+var score=0
+
+function intro(){
+    console.log("================================")
+    console.log("    CLI Premier League Quiz     ");
+    console.log("================================")
+    console.log("\n");
+    console.log("WELCOME to CLI-Quiz !!!");
+    var profile = readlineSync.question("Hello, what is your name and what football club do you support? ");
+    player = String(profile)
+    lst = player.split(" ");
+    console.log("\n");
+    console.log("PLAYER PROFILE:\nName: ",lst[0]);
+    if(lst.length >2)
+        console.log("Club: ",lst[1],lst[2]); // for club names with 2 words
+    else
+        console.log("Club: ",lst[1]);
+    console.log("\nLet's begin with the QUIZ !!!\n");
+
+}
+
+function QuesRun(question,answer){
+    var ans = readlineSync.question(question);
+    if(ans.toLowerCase()==answer){
+        console.log("Correct !");
+        score +=1;
+    }else{
+        console.log("Incorrect :(");
+    }
+    // console.log("Score: ",score);
+}
+
+function level1(){
+
+    console.log("================================")
+    console.log("    Level 1     ");
+    console.log("================================")
+
+    var Q_0 = [{
+        Q: 'How many teams are in the premier league? ',
+        A: '20'},{
+        Q: 'How many titles have Manchester united won? ',
+        A: '13'},{
+        Q: 'What position does David de gea play in? ',
+        A: 'goalkeeper'},{
+        Q: 'Who is the manager of Manchester City? ',
+        A: 'pep guardiola'},{
+        Q: 'In 2019, Romelu Lukaku left Man-Utd for which club? ',
+        A: 'inter milan'},{
+        Q: 'Arsene wenger was the manager of which football club? ',
+        A: 'arsenal'},{
+        Q: 'Mo Salah plays for which football club? ',
+        A: 'liverpool'},{
+        Q: 'St. James park is the home ground of which football club? ',
+        A: 'newcastle united'
+    }]
+
+    for(var i=0;i<5;i++){
+        var x = Math.floor(Math.random() * Q_0.length);
+        QuesRun(Q_0[x].Q,Q_0[x].A);
+        Q_0.splice(x,1);
+    }
+
+    if (score>=3){
+        console.log("\nCongrats ... You have cleared level 1\n");
+        return 1;
+    }else{
+        console.log("\nYou need to get 3 or more questions correct, try the quiz again (Ctrl+C to exit)\n");
+        level1()
+    }
+}
+
+function level2(){
+    console.log("================================")
+    console.log("    Level 2     ");
+    console.log("================================")
+    var Q_1 = [{
+        Q: 'How many teams get relegated in each season of the PL? ',
+        A: '3'},{
+        Q: 'Which Real madrid winger previously played for Tottenham hotspur? ',
+        A: 'gareth bale'},{
+        Q: 'Who is the all time premier league top scorer? ',
+        A: 'alan shearer'},{
+        Q: 'Did Luka modric ever play in the premier league? ',
+        A: 'yes'},{
+        Q: 'Which team won the title in 2016 against all odds? ',
+        A: 'leicester'}]
+
+    for(var i=0;i<Q_1.length;i++){
+        QuesRun(Q_1[i].Q,Q_1[i].A);
+    }
+}
+
+function main(){
+    intro();
+    level1();
+    level2();
+    console.log("\nFinal Score: "+score)
+}
+
+main();
